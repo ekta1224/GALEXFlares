eaff336db3a44b33b5796088abb5484a52cfb829
Ekta Patel <ep1091@nyu.edu>
2014-08-14 00:25:51 +0000

    reoraganize

--- code/step_model.py
+++ code/step_model.py
@@ -3,7 +3,8 @@ matplotlib.use('Agg')
 import matplotlib.pylab as plt
 matplotlib.rc('text', usetex=True)
 import numpy as np
-from flare_model import flare_model
+from fakedata import *
+from scipy.optimize import fmin
 
 def step(width, t0, height, rate, data):
     model = len(data) * [rate]
@@ -14,7 +15,8 @@ def step(width, t0, height, rate, data):
     model[box] = height
     return model
 
-def ln_like(width, tstep, data):
+def ln_like(pars, data):
+    width, tstep = pars
     data = np.array(sorted(data))
     w = width
     t0 = tstep-data[0]
@@ -25,7 +27,8 @@ def ln_like(width, tstep, data):
     b = Nout/(t0+t1)
     return Nout*np.log(b) + Nin*np.log(h) - b*(t0+t1) - h*w
 
-def get_hb(width, tstep, data):
+def get_hb(pars,data):
+    width, tstep = pars
     data = np.array(sorted(data))
     w = width
     t0 = tstep-data[0]
@@ -35,66 +38,73 @@ def get_hb(width, tstep, data):
     h = Nin/w
     b = Nout/(t0+t1)
     return h,b
-    
 
-width = 6
-tstep = 47
-# height = 100
-# rate = 1
-# data = np.arange(0,100,1.)
-# stepfn = step(width, tstep, height, rate, data)
+if __name__ == "__main__":    
+    width = 6
+    tstep = 47
 
-# plt.figure()
-# plt.plot(data,stepfn)
-# plt.savefig('step_model.pdf')
+    plt.figure(figsize = (8,12))
+    fakedata = photons(0.1, 100, 1, 1, 100, 50)
+    print len(fakedata)
+    fakedata = np.array(fakedata)
+    plt.subplot(411)
+    lls = []
+    widths = np.arange(1, 25, .1)
+    #optimization test
+    pars = [width, tstep] 
+    opt = fmin(ln_like, x0=pars, args=([fakedata]))
+    print opt[0], opt[1]
+    pars = [opt[0], opt[1]]
+    #print fmin(get_hb, x0=pars, args=([fakedata]))
+    print get_hb(pars, fakedata)
+    assert False
 
-plt.figure(figsize = (8,12))
-fakedata = flare_model(0.1, 100, 1, 1, 100, 50)
-print len(fakedata)
-fakedata = np.array(fakedata)
-plt.subplot(411)
-lls = []
-widths = np.arange(1, 25, .1)
+    for w in widths:
+        ll = ln_like(w, tstep, fakedata)
+        lls.append(ll)
 
-for w in widths:
-    ll = ln_like(w, tstep, fakedata)
-    lls.append(ll)
-#print heights, lls
-#assert False
-plt.xlabel('step width')
-plt.ylabel('likelihood')
-plt.plot(widths, lls)
+    plt.xlabel('step width')
+    plt.ylabel('likelihood')
+    plt.plot(widths, lls)
+    ymin = np.sort(lls)[np.isfinite(np.sort(lls))][0]
+    ymax = np.sort(lls)[np.isfinite(np.sort(lls))][-1]
+    plt.vlines(x=width, ymin=ymin, ymax=ymax, colors='red', linestyle='--')
+    plt.subplot(412)
+    lls = []
+    bs = []
+    heights = []
+    tsteps = np.arange(0,100,.1)
 
-plt.subplot(412)
-lls = []
-bs = []
-heights = []
-tsteps = np.arange(0,100,.1)
+    for t in tsteps:
+        ll = ln_like(width, t, fakedata)
+        lls.append(ll)
 
-for t in tsteps:
-    ll = ln_like(width, t, fakedata)
-    lls.append(ll)
+        b = get_hb(width, t, fakedata)[1]
+        bs.append(b)
 
-    b = get_hb(width, t, fakedata)[1]
-    bs.append(b)
+        height = get_hb(width, t, fakedata)[0]
+        heights.append(height)
 
-    height = get_hb(width, t, fakedata)[0]
-    heights.append(height)
+    plt.plot(tsteps,lls)
+    ymin = np.sort(lls)[np.isfinite(np.sort(lls))][0]
+    ymax = np.sort(lls)[np.isfinite(np.sort(lls))][-1]
+    plt.vlines(x=tstep, ymin=ymin, ymax=ymax, colors='red', linestyle='--')
+    plt.xlabel('step start time')
+    plt.ylabel('likelihood')
+
+    plt.subplot(413)
+    hminusb = [h-b for h,b in zip(heights,bs)]
+    plt.plot(tsteps,hminusb)
+    ymin = np.sort(hminusb)[np.isfinite(np.sort(hminusb))][0]
+    ymax = np.sort(hminusb)[np.isfinite(np.sort(hminusb))][-1]
+    plt.vlines(x=tstep, ymin=ymin, ymax=ymax, colors='red', linestyle='--')
+    plt.xlabel(r'$t_{step}$')
+    plt.ylabel('h-b (step height)')
+
+    plt.subplot(414)
+    plt.hist(fakedata, bins=len(fakedata), alpha = 0.5)
+    plt.tight_layout()
+
+    plt.savefig('step_lnll.pdf')
 
 
-plt.plot(tsteps,lls)
-plt.xlabel('step start time')
-plt.ylabel('likelihood')
-
-plt.subplot(413)
-plt.plot(tsteps, [h-b for h,b in zip(heights,bs)])
-plt.xlabel(r'$t_{step}$')
-plt.ylabel('h-b (step height)')
-
-plt.subplot(414)
-plt.hist(fakedata, bins=len(fakedata), alpha = 0.5)
-plt.tight_layout()
-
-plt.savefig('step_lnll.pdf')
-    
- 
